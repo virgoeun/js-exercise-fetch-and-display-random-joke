e9263373222e97cbc222f650dbbe686dad25f26c
Alice <johndoe@example.com>
2023-08-28 15:42:36 +0200

    answer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,65 @@
 function fetchJokes() {
-  // code here :
+  const url = "https://official-joke-api.appspot.com/random_joke";
+
+  return fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
+console.log(fetchJokes());
+
 async function displayJokes() {
-  // code here :
+  try {
+    const joke = await fetchJokes();
+    console.log(joke);
+
+    const setUp = joke.setup;
+    const punchLine = joke.punchline;
+
+    const setUpString = document.createElement("p");
+    const punchLineString = document.createElement("p");
+
+    console.log(setUpString);
+
+    setUpString.innerHTML = setUp;
+    punchLineString.innerHTML = punchLine;
+    //p has joke & punchline (string)
+
+    const divElement = document.getElementById("joke-container");
+    divElement.appendChild(setUpString);
+    divElement.appendChild(punchLineString);
+  } catch (error) {
+    console.log(error);
+  }
 }
+displayJokes();
 
 // don't forget to invoke the function :
 // displayJokes()
+
+/*
+async function fetchAndDisplayImages() {
+  try {
+    const response = await fetch("https://api.spacexdata.com/v4/launches");
+    const data = await response.json();
+
+    data.forEach((launchObj) => {
+      const patchImage = launchObj.links.patch.small;
+      const imgElement = document.createElement("img");
+
+      imgElement.src = patchImage;
+      document.body.appendChild(imgElement);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+fetchAndDisplayImages();
+*/
